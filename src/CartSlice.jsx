8cad4b9c1b754e916c3efb037a155bef8663ab8b
Yousef3la2 <33986623+Yousef3la2@users.noreply.github.com>
2024-09-11 07:18:59 +0300

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,25 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            state.items.push({ name, image, cost, quantity: 1 });
+        }
     },
     removeItem: (state, action) => {
-    },
+      const item = action.payload;
+      state.items = state.items.filter((i) => i.name !== item.name);
+      },
     updateQuantity: (state, action) => {
-
-    
+      const {name, quantity} = action.payload;
+      const item = state.items.find((i) => i.name === name);
+      item.quantity = quantity;
+      if (item.quantity === 0) {
+        state.items = state.items.filter((i) => i.name !== name);
+      }
     },
   },
 });
